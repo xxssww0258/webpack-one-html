c5a29de60050d9362992910ccbb54c97cb674632
john <xxssww0258@sina.com>
2020-09-03 20:56:52 +0800

    demo bug修复

--- projects/demo1/main.js
+++ projects/demo1/main.js
@@ -1,7 +1,7 @@
 import 'style-loader!css-loader!html5-reset/assets/css/reset.css'
 import Vue from 'vue'
 import App from './App.vue'
-import './a.css'
+
 new Vue({
     render: (h) => h(App),
 }).$mount('#app')
